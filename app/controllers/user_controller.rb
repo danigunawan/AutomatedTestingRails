a7a0768cb2ab7c650d76e7f68ea1dab132816951
Layne Faler <laynefaler@gmail.com>
2018-09-22 12:10:30 -0700

    Added ruby frontend javascript string from all routes declared

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,2 +1,13 @@
 class UserController < ApplicationController
+
+    def getById
+        @user = User.find({ id: params[:id] })
+        render json: @user, status: 200
+    end
+
+    def getByEmail
+        @user = User.find({ id: params[:id] })
+        render json: @user, status: 200
+    end
+
 end

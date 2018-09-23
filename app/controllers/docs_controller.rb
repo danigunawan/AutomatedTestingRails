e47af7e595d1776972f8d96a9622f325c6fe4efb
Layne Faler <laynefaler@gmail.com>
2018-09-23 12:02:07 -0700

    Converting pug into erb then adding client javascript to application

--- app/controllers/docs_controller.rb
+++ app/controllers/docs_controller.rb
@@ -30,18 +30,56 @@ class DocsController < ApplicationController
                 camelCased += method.downcase
                 
                 tempRoute = route
+                params = []
 
                 tempRoute.split('/').each do |l|
                     cap = l[0]
                     if cap == ':'
                         temp = l.slice(1)
+                        params.push(temp)
                         camelCased += temp.capitalize + 'Param'
                     else
                         camelCased += l.capitalize       
                     end
                 end
 
-                allRoutes.push({ method: method, route: route, camelCased: camelCased })
+                routeHeader = ''
+                submitButtonColor = ''
+                if method == 'GET'
+                    routeHeader = 'bg-success text-white'
+                    submitButtonColor = 'btn btn-outline-success'
+                end
+
+                if method == 'POST'
+                    routeHeader = 'bg-info text-white'
+                    submitButtonColor = 'btn btn-outline-info'
+                end
+
+                if method == 'PATCH'
+                    routeHeader = 'bg-warning text-white'
+                    submitButtonColor = 'btn btn-outline-warning'
+                end
+
+                if method == 'PUT'
+                    routeHeader = 'bg-warning text-white'
+                    submitButtonColor = 'btn btn-outline-warning'
+                end
+
+                if method == 'DELETE'
+                    routeHeader = 'bg-danger text-white'
+                    submitButtonColor = 'btn btn-outline-danger'
+                end
+
+                allRoutes.push({
+                    method: method, 
+                    route: route, 
+                    params: params,
+                    routeHeader: routeHeader,
+                    submitButtonColor: submitButtonColor,
+                    camelCased: camelCased,
+                    allowParams: tempRoute.split(':').length > 1,
+                    allowBody: if method == 'GET' then false else method != 'DELETE' end,
+                })
             end
 
             return allRoutes

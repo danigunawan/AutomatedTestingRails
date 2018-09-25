737d3bd19216fc3b66d8e4df401da954b1db9cfa
Layne Faler <laynefaler@gmail.com>
2018-09-24 20:33:27 -0700

    Created automatic documentation

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,12 +1,12 @@
 class UserController < ApplicationController
 
     def getById
-        @user = User.find({ id: params[:id] })
+        @user = User.find(params[:id])
         render json: @user, status: 200
     end
 
     def getByEmail
-        @user = User.find({ id: params[:id] })
+        @user = User.find_by_email({ id: params[:email] })
         render json: @user, status: 200
     end
 
